e9cea7b7989403ec7fb4ba2fc4c5c2e7c89e4dea
kripa-rai-bugasura <kripa.rai@bugasura.io>
2026-09-25 13:23:08 +0530

    Add config file

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
-	$servername = "localhost";
-	$username = "php_user";
-	$password = "PHP@Bug26!";
-	$dbname = "socialNetwork";
-
+	$config = require 'config.php';
+	$servername = $config['servername'];
+	$username = $config['username'];
+	$password =  $config['password'];
+	$dbname = $config['dbname'];
 	// connect to the database
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
 	if(!$conn)
 	{
 		die("Connection failed: " .  mysqli_connect_error());
